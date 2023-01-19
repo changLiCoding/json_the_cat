61745f3109ae46a8132ee4aeeff513aa572795ef
javascriptsucks <evix1988@live.cn>
2023-01-19 16:26:09 -0600

    cat-fetching-refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,14 @@
 const request = require('request');
 const fs = require('fs');
 
-const catFetch = (breed) => {
+const fetchBreedDescription = function(breed, cb) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed.toLowerCase()}`,(err,res,body) => {
     if (err) throw new Error(err);
-
     const data = JSON.parse(body);
-    if (data.length === 0) throw new Error('No breed found. ');
-
-    console.log(data[0].description);
+    //if (data.length === 0) throw new Error('No breed found. ');
+    cb(err, data[0].description);
   });
 };
-//node breedFetcher.js whatever
-//node breedFetcher.js chartreux
-const argvs = process.argv.slice(2);
 
-catFetch(...argvs);
+module.exports = { fetchBreedDescription };
+
